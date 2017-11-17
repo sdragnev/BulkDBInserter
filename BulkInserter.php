1d0399cc37297751fb5dd4b372db04602d785297
Stan Dragnev <stan@dragnev.ca>
2017-11-17 10:17:14 -0500

    the guts

---
+++ BulkInserter.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * User: stan
+ * Created on: 2017-11-14
+ *
+ * Functionality to bulk-insert values into mysql using PDO.
+ */
+
+class BulkInserter {
+
+  /** @var  \PDO */
+  private $pdo;
+  private $batch = 100; // how many rows to insert at once
+  private $bufferSize;
+  private $tableParams;
+  private $tableName;
+  private $tableInsertQuery;
+  private $fields;
+  public $writesRequested = 0;
+  public $writesCompleted = 0;
+
+  /**
+   * BulkInserter constructor.
+   * @param $pdo  PDO
+   * @param $tableName  string  Which table we're inserting in.
+   * @param $fieldNames array   An array of fields we'll be inserting.
+   * @param $replace  bool      Whether to use REPLACE INTO instead of INSERT (only use if unique/primary keys are present)
+   */
+  public function __construct($pdo, $tableName, $fieldNames, $replace = false) {
+    $this->pdo = $pdo;
+    $this->tableName = $tableName;
+    $this->bufferSize = 0;
+    $this->tableParams = [];
+    $op = $replace ? "REPLACE" : "INSERT IGNORE";
+
+    // We need to backtick table field names since the number (#) character throws a syntax error (thanks cmsp7000!)
+    array_walk($fieldNames, function(&$field, $key) {
+      $field = '`' . $field . '`';
+    });
+    $this->fields = '(' . implode(',', array_fill(0, count($fieldNames), '?')) . ')'; // eg (?, ?, ?)
+    $this->tableInsertQuery = "$op INTO $tableName (" . implode(',', $fieldNames) . ') VALUES ';
+
+    // Useless for MyISAM but potentially a time-saver for other engines
+    //$this->pdo->beginTransaction();
+  }
+
+  /**
+   * Queue up an insert.
+   * @param $values
+   */
+  public function write($values) {
+    $this->tableParams = array_merge($this->tableParams, $values);
+    $this->bufferSize++;
+
+    // Flush the buffer
+    if ($this->bufferSize == $this->batch) {
+      $this->flush();
+    }
+  }
+
+  /**
+   * Force a write.
+   * @param $final  boolean Whether this will be the last operation for this object.
+   */
+  public function flush($final = false) {
+
+    if ($this->bufferSize > 0) {
+      // Only bother if we have anything to do
+
+      $query = $this->tableInsertQuery . implode(',', array_fill(0, $this->bufferSize, $this->fields));
+      $statement = $this->pdo->prepare($query);
+      $result = $statement->execute($this->tableParams);
+
+      if (!$result) {
+        echo $statement->errorInfo()[2] . "\n";
+      }
+
+      // We want these to be equal at the end (unless this is a REPLACE INTO)
+      $this->writesRequested += $this->bufferSize;
+      $this->writesCompleted += $statement->rowCount();
+
+      // Clean the buffer
+      $this->bufferSize = 0;
+      $this->tableParams = [];
+    }
+
+    // Might be worth enabling for non-MyISAM tables inserts
+    /*
+    if ($final) {
+      $this->pdo->commit();
+    }
+    */
+
+  }
+
+  /**
+   * Iterates over a data result and inserts each row into the table.
+   * @param $result PDOStatement
+   */
+  public function insertPDOResult($result) {
+
+    while ($row = $result->fetch(PDO::FETCH_NUM)) {
+      $this->write($row);
+    }
+    $this->flush(true); // flush the write buffers
+  }
+}
