dcf8d762132d34f2200c6d0ebfa479bb93a018c0
Stan Dragnev <stan@dragnev.ca>
2021-05-04 14:06:19 -0400

    Optional parameter for transactions and a better README

--- BulkInserter.php
+++ BulkInserter.php
@@ -1,119 +1,133 @@
 <?php
 /**
+ * User: stan
  * Created on: 2017-11-14
  *
- * Functionality to bulk-insert values into mysql using PDO.
+ * Functionality to bulk-insert values into a database table using PDO.
  */
 
-class BulkInserter {
+namespace Intranet\Services;
 
-  /** @var  \PDO */
-  private $pdo;
-  private $batch = 500; // how many rows to insert at once
-  private $bufferSize;
-  private $tableParams;
-  private $tableName;
-  private $tableInsertQuery;
-  private $fields;
-  public $writesRequested = 0;
-  public $writesCompleted = 0;
-  public $dieOnError = true;
+use PDO;
+use PDOStatement;
 
-  /**
-   * BulkInserter constructor.
-   * @param $pdo  \PDO
-   * @param $tableName  string  Which table we're inserting in.
-   * @param $fieldNames array   An array of fields we'll be inserting.
-   * @param $replace  bool      Whether to use REPLACE INTO instead of INSERT (only use if unique/primary keys are present)
-   * @param $dieOnError  bool   If false, instead of throwing an exception, an error is echoed and the process will continue
-   */
-  public function __construct($pdo, $tableName, $fieldNames, $replace = false, $dieOnError = true) {
-    $this->pdo = $pdo;
-    $this->tableName = $tableName;
-    $this->dieOnError = $dieOnError;
-    $this->bufferSize = 0;
-    $this->tableParams = [];
-    $op = $replace ? "REPLACE" : "INSERT IGNORE";
+class BulkInserter
+{
 
-    // We need to backtick table field names since the number (#) character throws a syntax error (thanks cmsp7000!)
-    array_walk($fieldNames, function(&$field, $key) {
-      $field = '`' . $field . '`';
-    });
-    $this->fields = '(' . implode(',', array_fill(0, count($fieldNames), '?')) . ')'; // eg (?, ?, ?)
-    $this->tableInsertQuery = "$op INTO $tableName (" . implode(',', $fieldNames) . ') VALUES ';
+    /** @var  PDO */
+    private $pdo;
+    private $batch = 500; // how many rows to insert at once
+    private $bufferSize;
+    private $tableParams;
+    private $tableInsertQuery;
+    private $fields;
+    private $totalCount;
+    public $writesRequested = 0;
+    public $writesCompleted = 0;
+    public $dieOnError = true;
+    public $transaction;
 
-    // Useless for MyISAM but potentially a time-saver for other engines
-    $this->pdo->beginTransaction();
-  }
+    /**
+     * BulkInserter constructor.
+     * @param PDO $pdo PDO connector.
+     * @param string $tableName Which table we're inserting into.
+     * @param array $fieldNames An array of field names we'll be inserting.
+     * @param bool $replace Whether to use REPLACE INTO instead of INSERT (only use if unique/primary keys are present)
+     * @param bool $dieOnError If false, instead of throwing an exception, an error is echoed and the process will continue
+     * @param bool $transaction Whether to use a transaction for writing.
+     */
+    public function __construct($pdo, $tableName, $fieldNames, $replace = false, $dieOnError = true, $transaction = true,
+                                $totalCount = null) {
+        $this->pdo = $pdo;
+        $this->dieOnError = $dieOnError;
+        $this->bufferSize = 0;
+        $this->transaction = $transaction;
+        $this->tableParams = [];
+        $this->totalCount = $totalCount;
+        $op = $replace ? "REPLACE" : "INSERT IGNORE";
 
-  /**
-   * Queue up an insert.
-   * @param array $values
-   */
-  public function write($values) {
-    array_push($this->tableParams, ...$values);
-    $this->bufferSize++;
+        // We need to backtick table field names since the number (#) character throws a syntax error (thanks cmsp7000!)
+        array_walk($fieldNames, function (&$field, $key) {
+            $field = '`' . $field . '`';
+        });
+        $this->fields = '(' . implode(',', array_fill(0, count($fieldNames), '?')) . ')'; // eg (?, ?, ?)
+        $this->tableInsertQuery = "$op INTO $tableName (" . implode(',', $fieldNames) . ') VALUES ';
 
-    // Flush the buffer
-    if ($this->bufferSize == $this->batch) {
-      $this->flush();
-    }
-  }
-
-  /**
-   * Force a write.
-   * @param $final  boolean Whether this will be the last operation for this object.
-   */
-  public function flush($final = false) {
-
-    if ($this->bufferSize > 0) {
-      // Only bother if we have anything to do
-
-      $query = $this->tableInsertQuery . implode(',', array_fill(0, $this->bufferSize, $this->fields));
-      $statement = $this->pdo->prepare($query);
-      $result = $statement->execute($this->tableParams);
-
-      if ($result === false) {
-        if ($this->dieOnError) {
-          $this->pdo->rollBack();
-          throw new \RuntimeException($statement->errorInfo()[2]);
-        } else {
-          echo $statement->errorInfo()[2] . "\n";
+        if ($this->transaction) {
+            $this->pdo->beginTransaction();
         }
-      }
-
-      // We want these to be equal at the end (unless this is a REPLACE INTO)
-      $this->writesRequested += $this->bufferSize;
-      $this->writesCompleted += $statement->rowCount();
-
-      // Clean the buffer
-      $this->bufferSize = 0;
-      $this->tableParams = [];
     }
 
-    if ($final) {
-      $this->pdo->commit();
-    }
-  }
+    /**
+     * Queue up an insert.
+     * @param $values
+     */
+    public function write($values) {
+        array_push($this->tableParams, ...$values);
+        $this->bufferSize++;
 
-  /**
-   * Iterates over a data result and inserts each row into the table.
-   * @param $result \PDOStatement
-   */
-  public function insertPDOResult($result) {
-
-    while ($row = $result->fetch(\PDO::FETCH_NUM)) {
-      $this->write($row);
+        // Flush the buffer
+        if ($this->bufferSize == $this->batch) {
+            $this->flush();
+        } else if (!empty($this->totalCount) && ($this->writesCompleted + $this->bufferSize) == $this->totalCount) {
+            // This is the final write and we know it
+            $this->flush(true);
+        }
     }
-    $this->flush(true); // flush the write buffers
-  }
-  
-  /**
-   * Sets the number of writes done at once.
-   * @param $num
-   */
+
+    /**
+     * Force a write.
+     * @param $final  boolean Whether this will be the last operation for this object.
+     */
+    public function flush($final = false) {
+
+        if ($this->bufferSize > 0) {
+            // Only bother if we have anything to do
+            $query = $this->tableInsertQuery . implode(',', array_fill(0, $this->bufferSize, $this->fields));
+            $statement = $this->pdo->prepare($query);
+            $result = $statement->execute($this->tableParams);
+
+            if ($result === false) {
+                if ($this->dieOnError) {
+                    $this->pdo->rollBack();
+                    throw new \RuntimeException($statement->errorInfo()[2]);
+                } else {
+                    echo $statement->errorInfo()[2] . "\n";
+                }
+            }
+
+            // We want these to be equal at the end (unless this is a REPLACE INTO)
+            $this->writesRequested += $this->bufferSize;
+            $this->writesCompleted += $statement->rowCount();
+
+            // Clean the buffer
+            $this->bufferSize = 0;
+            $this->tableParams = [];
+        }
+
+        if ($final && $this->transaction) {
+            $this->pdo->commit();
+        }
+
+    }
+
+    /**
+     * Iterates over a data result and inserts each row into the table.
+     * @param $result PDOStatement
+     */
+    public function insertPDOResult($result) {
+
+        while ($row = $result->fetch(PDO::FETCH_NUM)) {
+            $this->write($row);
+        }
+        $this->flush(true); // flush the write buffers
+    }
+
+    /**
+     * Sets the number of writes done at once.
+     * @param $num
+     */
     public function setBatch($num) {
-      $this->batch = $num;
+        $this->batch = $num;
     }
-
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # BulkDBInserter
-Helper class that can buffer multiple PDO writes into one query
+Helper class that can buffer multiple PDO writes into one query.
+
+Example usage, inserting 1200 records in the "accounts" table given an existing PDO connection object $pdo,
+500 (default) records at a time:
+
+```
+// Inserts 1200 records in 3 queries (500 + 500 + 200)
+$writer = new BulkInserter($pdo, "accounts", ["id", "name"]);
+// $writer->setBatch(600); // optionally set the buffer size to a different number
+
+for ($i = 0; $i < 1200; $i++) {
+    $writer->write([$i, "John $i"]);
+}
+
+$writer->flush(true);   // flushes the remaining buffer
+```
